eb87b8a578dee2bad7e24e21cbd45ebc38bb55b7
Steve Conway <sconway@scottlogic.co.uk>
2018-12-18 13:49:44 +0000

    Completed Day11

---
+++ src/main/kotlin/Day11/Day11.kt
@@ -0,0 +1,60 @@
+package Day11
+
+private val serialNumber = 4842
+
+fun main(args: Array<String>) {
+
+    val bestPowerSquare = getBestPowerSquare(serialNumber, 3, 300)
+
+    println("Best power is ${bestPowerSquare.power}, at ${bestPowerSquare.x},${bestPowerSquare.y},${bestPowerSquare.size}")
+}
+
+data class PowerSquare(val x: Int, val y: Int, val power: Int, val size: Int)
+
+fun getBestPowerSquare(serialNumber: Int, minSize :Int, maxSize: Int) : PowerSquare {
+    val grid = mutableListOf<MutableList<Int>>()
+
+    for (y in 1..300) {
+        for (x in 1..300) {
+            grid.add(mutableListOf())
+            grid[y - 1].add(calcPower(x, y, serialNumber))
+        }
+    }
+
+    var bestX = 0
+    var bestY = 0
+    var bestPower = -10000
+    var bestSize = 0
+
+    for (size in minSize..maxSize) {
+        println("Size=$size")
+        for (y in 1..(300 - size + 1)) {
+            for (x in 1..(300 - size + 1)) {
+                val power = calcSquarePower(grid, x, y, size)
+                if (power > bestPower) {
+                    bestPower = power
+                    bestX = x
+                    bestY = y
+                    bestSize = size
+                }
+            }
+        }
+    }
+    return PowerSquare(bestX, bestY, bestPower, bestSize)
+}
+
+fun calcPower(x: Int, y: Int, serialNumber: Int): Int {
+
+    val rackID = x + 10
+    val power = ((rackID * y) + serialNumber) * rackID
+    return ((power % 1000) / 100) - 5
+}
+
+fun calcSquarePower(grid: List<List<Int>>, x: Int, y: Int, size: Int) : Int {
+
+    var total = 0
+    for (ix in x..(x+size-1))
+        for (iy in y.. y+size-1)
+            total += grid[iy-1][ix-1]
+    return total
+}
---
+++ src/test/kotlin/Day11/Day11KtTest.kt
@@ -0,0 +1,33 @@
+package Day11
+
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import kotlin.test.assertEquals
+
+internal class Day11KtTest {
+
+    data class CalcPowerInput(val x: Int, val y: Int, val serialNumber: Int)
+
+    @TestFactory
+    fun `calculate power`() =
+        listOf(
+            CalcPowerInput(3, 5, 8) to 4,
+            CalcPowerInput(122, 79, 57) to -5,
+            CalcPowerInput(217, 196, 39) to 0,
+            CalcPowerInput(101, 153, 71) to 4
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("for ${input} got $expected") {
+                assertEquals(expected, calcPower(input.x, input.y, input.serialNumber))
+            }
+        }
+
+    @TestFactory
+    fun `calculate best power squares`() =
+        listOf(
+            18 to PowerSquare(33, 45, 29, 3)
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("for ${input} got $expected") {
+                assertEquals(expected, getBestPowerSquare(input, 3, 3))
+            }
+        }
+}
